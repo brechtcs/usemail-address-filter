6f315261c2a5af343e9d8fc40a6d1cb47aa8fd49
Brecht Savelkoul <brecht.savelkoul@alumni.lse.ac.uk>
2020-05-06 21:29:19 +0200

    Move Config class to `lib`

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var { Opt, hash, list, nothing, string } = require('stdopt')
+var Config = require('./lib/config')
 
 var OPTS_ERR = 'Invalid option for `usemail-address-filter`'
 var PHASE_ERR = 'Address filter must run in phase `from` or `to`'
@@ -27,30 +27,6 @@ module.exports.block = function (opts) {
 
     if (addresses.includes(address)) sendReject(session.phase, address)
     if (verify && (await verify(address))) sendReject(session.phase, address)
-  }
-}
-
-class Config extends Opt {
-  static parse (opts) {
-    return hash(opts, this.struct).map(config => {
-      config.addresses = list(config.addresses).or([]).value()
-      return config
-    })
-  }
-
-  static get struct () {
-    return {
-      addresses: [nothing, list.of(string)],
-      verify: [nothing, VerifyFunction]
-    }
-  }
-}
-
-class VerifyFunction extends Opt {
-  static parse (fn) {
-    if (typeof fn === 'function') {
-      return fn
-    }
   }
 }
 
